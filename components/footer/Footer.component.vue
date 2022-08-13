3c184e0e3554bc1c2ae994ec4b767ea06964d4e1
Asam237 <abbasalimokolo@gmail.com>
2022-08-13 11:35:28 +0100

    add footer

---
+++ components/footer/Footer.component.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="flex flex-col md:flex-row mx-4 md:mx-0">
+    <div class="w-full md:w-2/5">
+      <h4
+        class="text-base md:text-xl text-gray-200"
+        style="font-family: 'DynaPuff', cursive"
+      >
+        WoilaTchogou237
+      </h4>
+      <p class="text-gray-200 mt-2 md:mt-4 font-normal text-sm">
+        Lorem ipsum dolor sit amet, consetetur sadipscing elitr.
+      </p>
+    </div>
+    <div class="w-full md:w-1/5 mt-4 md:mt-0">
+      <h4 class="text-base md:text-xl text-gray-200">My Account</h4>
+      <ul class="mt-1 md:mt-4">
+        <p class="text-sm text-gray-200">Contact Us</p>
+        <p class="text-sm text-gray-200">abbasalimokolo@gmail.com</p>
+        <p class="text-sm text-gray-200">example@gmail.com</p>
+        <p class="text-sm text-gray-200">Call us: +123456</p>
+      </ul>
+    </div>
+    <div class="w-full md:w-1/5 mt-4 md:mt-0">
+      <h4 class="text-base md:text-xl text-gray-200">About Us</h4>
+      <ul class="mt-1 md:mt-4">
+        <p class="text-sm text-gray-200">Sign in</p>
+        <p class="text-sm text-gray-200">Track Orders</p>
+        <p class="text-sm text-gray-200">Register Now</p>
+      </ul>
+    </div>
+    <div class="w-full md:w-1/5 mt-4 md:mt-0">
+      <h4 class="text-base md:text-xl text-gray-200">Contact</h4>
+      <ul class="mt-1 md:mt-4">
+        <p class="text-sm text-gray-200">Customer Support</p>
+        <p class="text-sm text-gray-200">About us</p>
+        <p class="text-sm text-gray-200">Privacy</p>
+        <p class="text-sm text-gray-200">Terms and Conditions</p>
+      </ul>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  name: 'FooterComponent',
+}
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -7,13 +7,18 @@
       </div>
     </div>
     <div style="background: #654642">
-      <div class="container mx-auto py-6">
+      <div class="container mx-auto py-6 md:py-12">
         <JoinCommunityComponent />
       </div>
     </div>
     <div style="background: white">
       <div class="container mx-auto pt-12 md:pt-24 mb-8 md:mb-16">
         <CollectionsComponent />
+      </div>
+    </div>
+    <div style="background: #654642">
+      <div class="container mx-auto py-6">
+        <FooterComponent />
       </div>
     </div>
   </div>
